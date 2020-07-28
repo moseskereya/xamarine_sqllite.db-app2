9cc92fbb55c86a5bd1b5976de81296ee2d046f6d
Moses <lazaromoses77@gmail.com>
2020-07-28 15:00:50 +0300

    save changes

---
+++ App2/Home.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Home : ContentPage
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            ListView.ItemsSource = await App.Database.GetPeopleAsync();
+        }
+
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrWhiteSpace(nameEntry.Text) && !String.IsNullOrWhiteSpace(AgeEntry.Text) && !String.IsNullOrWhiteSpace(NationalityEntry.Text) && !String.IsNullOrWhiteSpace(countryEntry.Text))
+            {
+                await App.Database.SavePersonAsync(new Person
+                {
+                    Name = nameEntry.Text,
+                    Nationality = NationalityEntry.Text,
+                    Age = int.Parse(AgeEntry.Text),
+                    Location = countryEntry.Text
+                });
+
+                nameEntry.Text = AgeEntry.Text = countryEntry.Text = NationalityEntry.Text = string.Empty;
+                ListView.ItemsSource = await App.Database.GetPeopleAsync();
+            }
+        }
+    }
+}
---
+++ App2/Person.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App2
+{
+    public class Person
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string  Nationality { get; set; }
+        public string Location { get; set; }
+        public int Age { get; set; }
+    }
+}
